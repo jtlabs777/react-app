1acb78bf6cd363a0d86d22480987f790f7526c57
Jacin Labrador <49028521+jtlabs777@users.noreply.github.com>
2023-04-26 13:39:02 -0400

    Conditional Rendering - encapsulated conditional in a function. The benefit is you can now pass arguments to function to make it more dynamic

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,16 @@ function App() {
 
   items = [];
 
-const message = items.length === 0 ? <p>No item found</p> : null;
+
+
+const getMessage = () => {
+  return items.length === 0 ? <p>No item found</p> : null;
+}
 
   return (
     <>
       <h1>List</h1>
-      {message}
+      {getMessage()}
       <ul>
         {items.map((item) => (
           <li key={item} className="list-group-item">
