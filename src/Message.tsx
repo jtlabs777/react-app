8aaf3c9595657d3db190a7c94f270f98705b7c34
Jacin Labrador <49028521+jtlabs777@users.noreply.github.com>
2023-04-26 13:10:23 -0400

    Created different components, learned about code Fragment via import nad how ocmponents can only return a single JSX element

---
+++ src/Message.tsx
@@ -0,0 +1,13 @@
+// PascalCasing - capitalize the first letter of every word
+function Message() {
+    //JSX: JavaScript XML
+    const name = 'Jacin';
+
+    if (name)
+        return <h1>Hello {name}</h1>
+    return <h1>Hello World</h1>
+}
+
+
+export default Message;
+
