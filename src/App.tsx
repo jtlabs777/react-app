8e967856a4833d6a50836c339bada7342467c723
Jacin Labrador <49028521+jtlabs777@users.noreply.github.com>
2023-04-26 13:22:04 -0400

    Created a dynamic list. Rendered using javascript code inside of curly braces. li elements must have a unique key

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,23 @@ import ListGroup from "./components/ListGroup";
 
 
 function App() {
-  return (
- 
-    <>
+const items = [
+  'New York',
+  'San Francisco',
+  'Tokyo',
+  'London',
+  'Paris'
+]
+
+
+return (
+  
+  <>
       <h1>List</h1>
-      <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+      <ul>
+        {items.map(item => <li key={item} className="list-group-item">{item}</li>)}
       </ul>
-    </>
+  </>
   );
 }
 export default App;
