89f6875f081549fec0bf38f3fd437bd955cf36a8
Jacin Labrador <49028521+jtlabs777@users.noreply.github.com>
2023-04-26 17:35:50 -0400

    Refactored to make ListGroup its own componet

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,10 @@
 import { useState } from "react";
+import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  const [selectedIndex, setSelectedIndex] = useState(-1);
-
-  //Event handler
   return (
     <>
-      <h1>List</h1>
-      {items.length === 0 && <p>No item found</p>}
-      <ul className="list-group">
-        {items.map((item, index) => (
-          <li
-            key={item}
-            className={
-              selectedIndex === index
-                ? "list-group-item active"
-                : "list-group-item"
-            }
-            onClick={() => {
-              setSelectedIndex(index);
-            }}
-          >
-            {item}
-          </li>
-        ))}
-      </ul>
+      <ListGroup />
     </>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
+
 function ListGroup() {
-    return <h1>List Group</h1>
+    let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  //Event handler
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            key={item}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default ListGroup;
