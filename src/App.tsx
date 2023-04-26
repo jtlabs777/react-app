aae95ec980820601250764d55b7fdb5c3d139eda
Jacin Labrador <49028521+jtlabs777@users.noreply.github.com>
2023-04-26 13:33:22 -0400

    Conditional Rendering - another example that is slightly better than previous but can still be improved

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,12 @@ function App() {
 
   items = [];
 
-  if (items.length === 0) {
-    return (
-      <>
-        <h1>List</h1>
-        <p>No item found</p>
-      </>
-    );
-  }
+
 
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 ? <p>No item found</p> : null}
       <ul>
         {items.map((item) => (
           <li key={item} className="list-group-item">
